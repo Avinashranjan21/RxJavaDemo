80025dbb0f30dc6ffd16e5461a96177234e68550
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-02-15 10:36:29 +0530

    operator sample added

--- src/com/devschool/operators/BasicOperators.java
+++ src/com/devschool/operators/BasicOperators.java
@@ -13,6 +13,10 @@ public class BasicOperators {
         skipOperator();
         takeWhileOperator();
         skipWhileOperator();
+        distinctOperator();
+        distinctUntilChangedOperator();
+        elementAtOperator();
+        startWithOperator();
 
     }
 
@@ -55,6 +59,37 @@ public class BasicOperators {
         Observable.range(1, 100)
                 .skipWhile(i -> i <= 95)
                 .subscribe(i -> System.out.println("skipWhile RECEIVED: " + i));
+    }
+
+    private static void distinctOperator() {
+        System.out.println("distinct Operator.....................................");
+
+        Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+                .map(String::length)
+                .distinct()
+                .subscribe(i -> System.out.println("distinctOperator RECEIVED: " + i));
+    }
+
+    private static void distinctUntilChangedOperator() {
+        System.out.println("distinctUntilChanged Operator.....................................");
+
+        Observable.just(1, 1, 1, 2, 2, 3, 3, 2, 1, 1)
+                .distinctUntilChanged()
+                .subscribe(i -> System.out.println("distinctUntilChanged RECEIVED: " + i));
+
+    }
+
+    private static void elementAtOperator(){
+        System.out.println("distinctUntilChanged Operator.....................................");
+        Observable.just("Alpha", "Beta", "Zeta", "Eta", "Gamma", "Delta")
+                .elementAt(3)
+                .subscribe(i -> System.out.println("elementAtOperator RECEIVED: " + i));
+    }
+
+    private static void startWithOperator(){
+        Observable<String> menu = Observable.just("Coffee", "Tea", "Espresso", "Latte");
+        //print menu
+        menu.startWith("COFFEE SHOP MENU").subscribe(System.out::println);
     }
 
 }
