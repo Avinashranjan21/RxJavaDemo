6618ba36bc48c4729fc92269f3108930e2a3b1e8
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-02-16 13:54:04 +0530

    concurrency sample added

---
+++ src/com/devschool/Concurrency/ConcurrencyMain.java
@@ -0,0 +1,58 @@
+package com.devschool.Concurrency;
+
+
+import com.devschool.Utility;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Concurrency and Parallelization
+ */
+public class ConcurrencyMain {
+
+    public static void main(String[] args) {
+
+        Observable<String> source1 = Observable.just("Avinash", "Pandey", "Ranjan", "Bokaro", "Steel", "City")
+                .subscribeOn(Schedulers.computation())
+                .map(s -> intenseCalculation((s)));
+//                .subscribe(System.out::println);
+
+        Observable<Integer> source2 = Observable.range(1, 6)
+                .subscribeOn(Schedulers.computation())
+                .map(s -> intenseCalculation((s)));
+//                .subscribe(System.out::println);
+
+
+        System.out.println("Both subscriber are working on separate observable");
+
+
+        System.out.println("Now merging two observable into one observable using ZIP method");
+
+        Observable.zip(source1, source2, (s,i) -> s + "-" + i)
+                .subscribe(System.out::println);
+
+        Utility.sleep(20000);
+
+        blockingSubscribe();
+    }
+
+    private static <T> T intenseCalculation(T value) {
+//        Utility.sleep(ThreadLocalRandom.current().nextInt(3000)); //todo Making thread sleep for random time
+        Utility.sleep(1000); // TODO: 16/02/18 Making thread to sleep for 1 sec fixed
+        return value;
+    }
+
+    private static void blockingSubscribe(){
+        System.out.println("Calling blockingSubscribe() to block main thread");
+        Observable.just("Avinash", "Pandey", "Ranjan", "Bokaro", "Steel", "City")
+                .subscribeOn(Schedulers.computation())
+                .map(ConcurrencyMain::intenseCalculation)
+                .blockingSubscribe(System.out::println,
+                        Throwable::printStackTrace,
+                        () -> System.out.println("Done!"));
+    }
+
+}
--- src/com/devschool/combine_observables/CombineObservableType.java
+++ src/com/devschool/combine_observables/CombineObservableType.java
@@ -18,7 +18,6 @@ public class CombineObservableType {
         Observable<String> source1 = Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon");
         Observable<String> source2 = Observable.just("Zeta", "Eta", "Theta");
 
-        Observable.merge(source1, source2)
-                .subscribe(i -> System.out.println("RECEIVED: " + i));
+        Observable.merge(source1, source2).subscribe(i -> System.out.println("RECEIVED: " + i));
     }
 }
